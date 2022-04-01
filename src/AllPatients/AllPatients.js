47c6f9e02583521084e09800fa18389b608a558a
salkow <salkow@outlook.com>
2022-04-01 14:12:37 +0300

    Made the page responsive and improved the add patient button.

--- src/AllPatients/AllPatients.js
+++ src/AllPatients/AllPatients.js
@@ -2,51 +2,13 @@ import "./AllPatients.css";
 
 import axiosInstance from "../axios";
 
+import PatientCard from "./PatientCard/PatientCard.js";
+
 import { Link } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 
-const patients1 = [
-	{
-		firstname: "Kason",
-		lastname: "Frye",
-		patient_id: 2,
-		heart_rate: "75",
-		email: "kasfr@gmail.com",
-		z_accel: "1",
-		sex: "Female",
-		address_street: "Syggrou",
-		address_number: "189",
-		address_city: "Athens",
-		address_postalcode: "17673",
-	},
-	{
-		firstname: "John",
-		lastname: "Smith",
-		patient_id: 11,
-		heart_rate: "75",
-		sys_blood_pressure: "128",
-		dia_blood_pressure: "97",
-		email: "jsmith@gmail.com",
-		sex: "Male",
-		address_street: "Syggrou",
-		address_number: "189",
-		address_city: "Athens",
-		address_postalcode: "17673",
-	},
-	{
-		firstname: "Mary",
-		lastname: "Jones",
-		patient_id: 12,
-		heart_rate: "58",
-		email: "mjones@gmail.com",
-		sex: "Female",
-		address_street: "Syggrou",
-		address_number: "189",
-		address_city: "Athens",
-		address_postalcode: "17673",
-	},
-];
+import PersonAddTwoToneIcon from "@mui/icons-material/PersonAddTwoTone";
 
 const AllPatients = () => {
 	const [patients, setPatients] = useState([]);
@@ -62,52 +24,31 @@ const AllPatients = () => {
 	}, []);
 
 	return (
-		<div className="cards">
-			{patients.map((patient, index) => (
-				<div className="card" key={index}>
-					<table>
-						<tbody>
-							<tr>
-								<td>
-									Name: {patient.firstname} {patient.lastname}
-								</td>
-							</tr>
-
-							<tr>
-								<td>Heart Rate: {patient.heart_rate} bpm</td>
-							</tr>
-
-							<tr>
-								<td>Gender: {patient.sex} </td>
-							</tr>
-
-							{patient.sys_blood_pressure &&
-								patient.dia_blood_pressure && (
-									<tr>
-										<td>
-											Blood Pressure:{" "}
-											{patient.sys_blood_pressure} /{" "}
-											{patient.dia_blood_pressure}
-										</td>
-									</tr>
-								)}
-
-							{patient.z_accel && (
-								<tr>
-									<td>
-										Z Accel: {patient.z_accel} m/s
-										<sup>2</sup>
-									</td>
-								</tr>
-							)}
-						</tbody>
-					</table>
-					<br />
-					<Link to={`/patient/${patient.patient_id}`}>Details</Link>
-				</div>
-			))}
-
-			<Link to={"/add-patient/"}>Add Patient</Link>
+		<div>
+			<div className="add-patient-container">
+				<Link
+					className="right shadow rounded-corners no-u"
+					to={"/add-patient/"}
+				>
+					<PersonAddTwoToneIcon className="all-patient-card-icon" />{" "}
+					Add Patient
+				</Link>
+			</div>
+			<div className="all-patient-cards">
+				{patients.map((patient) => (
+					<PatientCard
+						key={patient.patient_id}
+						patient_id={patient.patient_id}
+						firstName={patient.firstname}
+						lastName={patient.lastname}
+						heart_rate={patient.heart_rate}
+						sex={patient.sex}
+						sys_blood_pressure={patient.sys_blood_pressure}
+						dia_blood_pressure={patient.dia_blood_pressure}
+						z_accel={patient.z_accel}
+					/>
+				))}
+			</div>
 		</div>
 	);
 };
