ab45667a873813d7363384e6b16163434b63aaf4
salkow <salkow@outlook.com>
2022-04-02 15:36:23 +0300

    Stopped the data from being loaded every time we go to the all patients page.

--- src/AllPatients/AllPatients.js
+++ src/AllPatients/AllPatients.js
@@ -8,21 +8,21 @@ import PatientCard from "./PatientCard/PatientCard.js";
 
 import { Link } from "react-router-dom";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import PersonAddTwoToneIcon from "@mui/icons-material/PersonAddTwoTone";
 
-const AllPatients = () => {
-	const [patients, setPatients] = useState([]);
-
+const AllPatients = ({ patients, setPatients }) => {
 	useEffect(() => {
-		let user_id = localStorage.getItem("user_id");
+		if (!patients) {
+			let user_id = localStorage.getItem("user_id");
 
-		// TODO: Set user_id from login.
+			// TODO: Set user_id from login.
 
-		axiosInstance.get(`patients?user_id=18`).then((res) => {
-			setPatients(res.data);
-		});
+			axiosInstance.get(`patients?user_id=18`).then((res) => {
+				setPatients(res.data);
+			});
+		}
 	}, []);
 
 	return (
@@ -39,21 +39,23 @@ const AllPatients = () => {
 					Add Patient
 				</Link>
 			</div>
-			<div className="all-patient-cards">
-				{patients.map((patient) => (
-					<PatientCard
-						key={patient.patient_id}
-						patient_id={patient.patient_id}
-						firstName={patient.firstname}
-						lastName={patient.lastname}
-						heart_rate={patient.heart_rate}
-						sex={patient.sex}
-						sys_blood_pressure={patient.sys_blood_pressure}
-						dia_blood_pressure={patient.dia_blood_pressure}
-						z_accel={patient.z_accel}
-					/>
-				))}
-			</div>
+			{patients && (
+				<div className="all-patient-cards">
+					{patients.map((patient) => (
+						<PatientCard
+							key={patient.patient_id}
+							patient_id={patient.patient_id}
+							firstName={patient.firstname}
+							lastName={patient.lastname}
+							heart_rate={patient.heart_rate}
+							sex={patient.sex}
+							sys_blood_pressure={patient.sys_blood_pressure}
+							dia_blood_pressure={patient.dia_blood_pressure}
+							z_accel={patient.z_accel}
+						/>
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Login from "./Login/Login.js";
 import AddPatient from "./AddPatient/AddPatient.js";
 import Navbar from "./Navbar/Navbar.js";
 
+import { useState } from "react";
+
 import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 
 function App() {
+	const [patients, setPatients] = useState();
+
 	return (
 		<BrowserRouter>
 			{window.location.pathname !== "/login" && <Navbar />}
 			<Routes>
 				<Route path="/login" element={<Login />} />
-				<Route path="/" element={<AllPatients />} />
+				<Route
+					path="/"
+					element={
+						<AllPatients
+							patients={patients}
+							setPatients={setPatients}
+						/>
+					}
+				/>
 				<Route path="/patient/:id" element={<Patient />} />
 				<Route path="/add-patient" element={<AddPatient />} />
 			</Routes>
